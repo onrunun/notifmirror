a1a9e9cde8d04e93f69b79205ae7fc6abaf28718
S. Onur Cetin <onrunun@users.noreply.github.com>
2026-08-11 17:01:08 +0300

    Release 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.notifmirror.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     signingConfigs {
